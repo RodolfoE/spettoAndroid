c9a96c9f7a08c6fc0d21b56a1825bb242657b53a
Rodolfo Eliezer S. Rezende <Rodolfo@contabilidadebessa.com.br>
2019-12-18 17:01:26 -0300

    'as'

--- app/src/main/java/com/example/myapplication/EscolherProdutoActivity.java
+++ app/src/main/java/com/example/myapplication/EscolherProdutoActivity.java
@@ -40,33 +40,18 @@ public class EscolherProdutoActivity extends AppCompatActivity {
     }
 
     private void fetchWeatherDetails() {
-        //Obtain an instance of Retrofit by calling the static method.
         Retrofit retrofit = NetworkClient.getRetrofitClient();
-        /*
-        The main purpose of Retrofit is to create HTTP calls from the Java interface based on the annotation associated with each method. This is achieved by just passing the interface class as parameter to the create method
-        */
         EscolherProdutosAPI produtos = retrofit.create(EscolherProdutosAPI.class);
-        /*
-        Invoke the method corresponding to the HTTP request which will return a Call object. This Call object will used to send the actual network request with the specified parameters
-        */
         Call call = produtos.getProdutos();
-        /*
-        This is the line which actually sends a network request. Calling enqueue() executes a call asynchronously. It has two callback listeners which will invoked on the main thread
-        */
         call.enqueue(new Callback() {
             @Override
             public void onResponse(Call call, Response response) {
-                /*This is the success callback. Though the response type is JSON, with Retrofit we get the response in the form of WResponse POJO class
-                 */
                 if (response.body() != null) {
                     Produto produto= (Produto) response.body();
                 }
             }
             @Override
             public void onFailure(Call call, Throwable t) {
-                /*
-                Error callback
-                */
             }
         });
     }
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        startActivity(new Intent(this, EscolherProdutoActivity.class));
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
